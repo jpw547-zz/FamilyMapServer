435d88ee17704cef015f8a4e6bf784acbf84444d
John Werner <jwerner547@gmail.com>
2018-02-12 10:16:46 -0700

    successfully added rows to the database

--- src/database/SQLiteJDBC.java
+++ src/database/SQLiteJDBC.java
@@ -10,10 +10,26 @@ public class SQLiteJDBC {
          Class.forName("org.sqlite.JDBC");
          //SQLiteJDBC jdbc = new SQLiteJDBC();
          c = DriverManager.getConnection("jdbc:sqlite:fmdb.db");
+         c.setAutoCommit(false);
       } catch ( Exception e ) {
          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
          System.exit(0);
       }
       System.out.println("Opened database successfully");
+      
+      try {
+    	  Statement stmt = c.createStatement();
+          String sql = "INSERT INTO USERS (userName, password, email, firstName, lastName, gender) " + "VALUES ('klinc9', 'testy', 'andrew_werner@byu.edu', 'Andrew', 'Werner', 'M');"; 
+          stmt.executeUpdate(sql);
+          
+          stmt.close();
+          c.commit();
+          c.close();
+      } catch(SQLException s) {
+    	  System.err.println( s.getClass().getName() + ": " + s.getMessage() );
+          System.exit(0);
+      }
+      
+      System.out.println("Records created successfully");
    }
 }
