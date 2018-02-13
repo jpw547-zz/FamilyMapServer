26fee75aef272777d1d79f679b39154e2547f870
John Werner <jwerner547@gmail.com>
2018-02-12 17:43:22 -0700

    added a server class

---
+++ src/server/Server.java
@@ -0,0 +1,18 @@
+package server;
+
+public class Server {
+	public static void main() {
+		
+		// *** HANDLERS ***
+		// /user/register
+		// /user/login
+		// /clear
+		// /fill/[username]/{generations}
+		// /load
+		// /person/[personID]
+		// /person
+		// /event/[eventID]
+		// /event
+		
+	}
+}
